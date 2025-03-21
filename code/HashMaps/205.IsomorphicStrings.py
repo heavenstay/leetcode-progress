e6b03d18a333801f99f71036dd4557ccad4a8e40
heavenstay <even.soulardu49@gmail.com>
2025-03-21 09:04:43 +0100

    Refactor isIsomorphic function to use a single hashmap for character mapping

--- code/HashMaps/205.IsomorphicStrings.py
+++ code/HashMaps/205.IsomorphicStrings.py
@@ -3,20 +3,18 @@
 # Time Taken: 20 min
 # Attempts: 2
 # Hints Used: Yes
-# Notes: Used two dictionaries to map the characters in the two strings. If the mapping is not the same, return False.
+# Notes: Use a hashmap to store the mapping of characters from s to t. If a character in s is already in the hashmap, check if the mapping is the same as the current character in t. If not, return False. If the current character in t is already in the hashmap, return False. Otherwise, add the mapping to the hashmap.
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        map_s = {}
-        map_t = {}
+        hash_map = {}
 
         for i in range(0, len(s)):
-            if s[i] not in map_s: 
-                map_s[s[i]] = i
+            if s[i] in hash_map: 
+                if t[i] != hash_map[s[i]]:
+                    return False
+            elif t[i] in hash_map.values():
+                return False 
             
-            if t[i] not in map_t: 
-                map_t[t[i]] = i
+            hash_map[s[i]] = t[i]
 
-            if map_s[s[i]] != map_t[t[i]]:
-                return False
-        
-        return True
+        return True 
